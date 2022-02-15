21767862996819b90973a4fbbea8fa86d41395b8
linhai <kami@cancanyou.com>
2022-02-15 11:20:16 +0800

    Update runtime name 108

--- runtime/mars/src/network/part_session.rs
+++ runtime/mars/src/network/part_session.rs
@@ -22,7 +22,7 @@ impl pallet_session::Config for Runtime {
     type ValidatorIdOf = pallet_collator_selection::IdentityCollator;
     type ShouldEndSession = pallet_session::PeriodicSessions<Period, Offset>;
     type NextSessionRotation = pallet_session::PeriodicSessions<Period, Offset>;
-    type SessionManager = CollatorSelection;
+    type SessionManager = OracleFinance;//CollatorSelection;
     // Essentially just Aura, but lets be pedantic.
     // type SessionHandler = <SessionKeys as sp_runtime::traits::OpaqueKeys>::KeyTypeIdProviders;
     type SessionHandler = <SessionKeys as sp_runtime::traits::OpaqueKeys>::KeyTypeIdProviders;
