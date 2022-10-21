c1f57d4fa9f04f5059811d9b3d704e7c45624063
kami1983 <kami@cancanyou.com>
2022-10-21 13:35:08 +0800

    Update 0.9.28

--- runtime/mars/src/network/part_authorship.rs
+++ runtime/mars/src/network/part_authorship.rs
@@ -17,5 +17,5 @@ impl pallet_authorship::Config for Runtime {
     type FindAuthor = pallet_session::FindAccountFromAuthorIndex<Self, Aura>;
     type UncleGenerations = UncleGenerations;
     type FilterUncle = ();
-    type EventHandler = (CollatorSelection,);
+    type EventHandler = (CollatorSelection, AresOracle);
 }
